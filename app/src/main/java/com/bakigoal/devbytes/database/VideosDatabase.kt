0abe3c3c6787ee4bbed7058f9cd07c33417e182d
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-18 12:36:09 +0500

    extract initialization method

--- app/src/main/java/com/bakigoal/devbytes/database/VideosDatabase.kt
+++ app/src/main/java/com/bakigoal/devbytes/database/VideosDatabase.kt
@@ -10,20 +10,31 @@ import com.bakigoal.devbytes.database.entity.VideoEntity
 @Database(entities = [VideoEntity::class], version = 1)
 abstract class VideosDatabase : RoomDatabase() {
     abstract val videoDao: VideoDao
-}
 
-private lateinit var INSTANCE: VideosDatabase
+    companion object {
 
-fun getDatabase(context: Context): VideosDatabase {
-    synchronized(VideosDatabase::class.java) {
-        if (!::INSTANCE.isInitialized) {
-            INSTANCE = Room.databaseBuilder(
-                context.applicationContext,
-                VideosDatabase::class.java,
-                "videos"
-            )
-                .build()
+        @Volatile
+        private lateinit var INSTANCE: VideosDatabase
+
+        fun getDatabase(context: Context): VideosDatabase {
+            if (::INSTANCE.isInitialized) {
+                return INSTANCE
+            }
+            initDb(context)
+            return INSTANCE
+        }
+
+        private fun initDb(context: Context) {
+            synchronized(this) {
+                if (!::INSTANCE.isInitialized) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        VideosDatabase::class.java,
+                        "videos"
+                    )
+                        .build()
+                }
+            }
         }
     }
-    return INSTANCE
 }
