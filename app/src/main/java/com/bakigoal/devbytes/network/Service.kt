24aa58c483f6e4693949c74a61bdf583398c4001
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-18 10:58:41 +0500

    extract URL

--- app/src/main/java/com/bakigoal/devbytes/network/Service.kt
+++ app/src/main/java/com/bakigoal/devbytes/network/Service.kt
@@ -20,6 +20,8 @@ interface DevbyteService {
     fun getPlaylist(): Deferred<NetworkVideoContainer>
 }
 
+private const val URL = "https://devbytes.udacity.com/"
+
 /**
  * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
  * full Kotlin compatibility.
@@ -34,7 +36,7 @@ private val moshi = Moshi.Builder()
 object Network {
     // Configure retrofit to parse JSON and use coroutines
     private val retrofit = Retrofit.Builder()
-            .baseUrl("https://devbytes.udacity.com/")
+            .baseUrl(URL)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
