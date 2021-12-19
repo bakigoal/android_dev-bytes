8e93b4ed8a93b1d408142655fd1e606cd358346e
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-19 11:42:29 +0500

    create repository that fetch playlist and sore it in db

--- app/src/main/java/com/bakigoal/devbytes/repository/VideosRepository.kt
+++ app/src/main/java/com/bakigoal/devbytes/repository/VideosRepository.kt
@@ -1,1 +1,20 @@
 package com.bakigoal.devbytes.repository
+
+import com.bakigoal.devbytes.database.VideosDatabase
+import com.bakigoal.devbytes.network.Network
+import com.bakigoal.devbytes.network.dto.asDbModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Repository for fetching devbytes from the network and storing them on disk
+ */
+class VideosRepository (private val database: VideosDatabase){
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylistAsync().await()
+            database.videoDao.insertAll(*playlist.asDbModel())
+        }
+    }
+}
