cdbc3a1a17ba9dae35a16a20947f67cddbf12357
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-19 12:36:25 +0500

    add Worker, which executes job once a day

--- app/src/main/java/com/bakigoal/devbytes/DevByteApplication.kt
+++ app/src/main/java/com/bakigoal/devbytes/DevByteApplication.kt
@@ -1,12 +1,22 @@
 package com.bakigoal.devbytes
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.bakigoal.devbytes.work.RefreshDataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
  */
 class DevByteApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     /**
      * onCreate is called before the first screen is shown to the user.
@@ -17,5 +27,25 @@ class DevByteApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupRefreshDataWorker()
+        }
+    }
+
+    private fun setupRefreshDataWorker() {
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(
+            1,
+            TimeUnit.DAYS
+        ).build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWork.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
     }
 }
--- app/src/main/java/com/bakigoal/devbytes/work/RefreshDataWork.kt
+++ app/src/main/java/com/bakigoal/devbytes/work/RefreshDataWork.kt
@@ -1,1 +1,28 @@
 package com.bakigoal.devbytes.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.bakigoal.devbytes.database.VideosDatabase
+import com.bakigoal.devbytes.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWork(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = VideosDatabase.getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            // retry this Job in the future
+            Result.retry()
+        }
+    }
+}
